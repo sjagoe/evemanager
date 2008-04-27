46414d6fc5625cd98ef7662e470cbed8e842e29b
Simon Jagoe <azriphale@gmail.com>
2008-04-27 18:14:35 +0100

    Adding EveManagerCharacterWidget to the project

---
+++ EVE_Manager_gui/include/evemanagercharacterwidget.hh
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2008 Simon Jagoe
+ *
+ * This file is part of <<<<>>>>.
+ *
+ * <<<<>>>> is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * <<<<>>>> is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with <<<<>>>>.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef EVEMANAGERCHARACTERWIDGET_HH_INCLUDED
+#define EVEMANAGERCHARACTERWIDGET_HH_INCLUDED
+
+#include <QWidget>
+
+namespace em_gui
+{
+    class EveManagerCharacterWidget: public QWidget
+    {
+        Q_OBJECT
+    };
+};
+
+#endif // EVEMANAGERCHARACTERWIDGET_HH_INCLUDED
